ab564c0390462990c69322cd7730bcddeabdfc56
funkill2 <funkill2@gmail.com>
2019-03-19 19:48:06 +0300

    fmt

--- fprint-sys/src/lib.rs
+++ fprint-sys/src/lib.rs
@@ -1,9 +1,14 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+#![warn(clippy::all)]
 
 mod bindings {
-    #[allow(unused_variables)]
+    #![allow(
+        non_upper_case_globals,
+        non_camel_case_types,
+        non_snake_case,
+        unused_variables,
+        clippy::unreadable_literal,
+        clippy::const_static_lifetime
+    )]
     include!(concat!(env!("OUT_DIR"), "/fprint.rs"));
 }
 
